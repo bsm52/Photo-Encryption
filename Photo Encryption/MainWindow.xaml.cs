44fbc2d8ce3a59091353d06801f63807b16f2050
Brandon Meier <bsm52@zips.uakron.edu>
2020-03-21 11:36:06 -0400

    save the image

--- Photo Encryption/MainWindow.xaml.cs
+++ Photo Encryption/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +13,6 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Photo_Encryption
 {
@@ -23,6 +24,50 @@ namespace Photo_Encryption
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void FileUploadBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // holds the name of the file given by the user
+            string filename = "";
+            
+            // prompt for the file
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            if(openFileDialog.ShowDialog() == true)
+            {
+                filename = openFileDialog.FileName;
+            }
+
+            // create a temp directory to hold the image if one doesn't exist
+            if(!System.IO.Directory.Exists(Path.Combine(Environment.CurrentDirectory, "temp")))
+            {
+                System.IO.Directory.CreateDirectory(Path.Combine(Environment.CurrentDirectory, "temp"));
+            }
+
+            // copy the chosen file from the user to the temp directory
+            System.IO.File.Copy(filename, Path.Combine(Environment.CurrentDirectory, "temp", Path.GetFileName(filename)));
+
+            // make the new filename's path in the temp directory
+            filename = Path.Combine(Environment.CurrentDirectory, "temp", Path.GetFileName(filename));
+
+            MessageBox.Show(filename);
+        }
+        
+        private void DisplayImagePreview(string filename)
+        {
+
+        }
+
+        private void ChangeStyleBtn_Click(object sender, RoutedEventArgs e)
+        {
+            StyleChanger.ChangeLabelSize(60);
+        }
+
+        private void Newindowbtn_Click(object sender, RoutedEventArgs e)
+        {
+            Testwindow test = new Testwindow();
+            test.Show();
         }
     }
 }
---
+++ Photo Encryption/StyleChanger.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.IO;
+using Microsoft.Win32;
+using System.Windows.Media;
+
+namespace Photo_Encryption
+{
+   public static class StyleChanger
+    {
+        public static double LabelSize = 0;
+        public static Brush LabelColor = Brushes.Lime;
+
+        public static void ChangeFontColor(Brush color)
+        {
+            LabelColor = color;
+
+            Style style = new Style { TargetType = typeof(Label) };
+
+            style.Setters.Add(new Setter(Control.FontSizeProperty, LabelSize));
+            style.Setters.Add(new Setter(Label.BackgroundProperty, LabelColor));
+
+            Application.Current.Resources["LabelStyle"] = style;
+        }
+
+        public static void ChangeLabelSize(double size)
+        {
+            LabelSize = size;
+
+            Style style = new Style { TargetType = typeof(Label) };
+
+            style.Setters.Add(new Setter(Control.FontSizeProperty, size));
+            style.Setters.Add(new Setter(Label.BackgroundProperty, LabelColor));
+
+            Application.Current.Resources["LabelStyle"] = style;
+        }
+    }
+}
